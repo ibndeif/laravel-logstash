ff136fec1083f683c921b9c82d62fa9baf5ff83b
Ahmed Deif <a.deif@namaa.solutions.com>
2024-02-04 19:13:42 +0200

    fix dataset

--- src/Middleware/LoggerMiddleware.php
+++ src/Middleware/LoggerMiddleware.php
@@ -35,9 +35,6 @@ class LoggerMiddleware
                 'domain' => $request->getHost(),
                 'full' => url()->current(),
                 'path' => $request->path()
-            ],
-            'event'=>[
-                'dataset'=>'Dashboard'
             ]
         ];
 
